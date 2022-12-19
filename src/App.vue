933650e3b0bd7383ed8dbcb4f8c41210e652fb4a
interpreterK <rhpidfyre@tuta.io>
2022-12-18 21:12:25 -0500

    Commit all to github

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <Header/>
+    
+</template>
+
+<script>
+import Header from './Header.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Header
+    }
+}
+</script>
+
+<style lang="sass">
+    @use "./SASS/App.sass"
+</style>
---
+++ src/Header.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <div class="Top-Bar">
+            <a href="../public/index.html">
+                <h1>rhpidfyre</h1>
+            </a>
+            <nav>
+                <ul class="Nav-Links">
+                    <a href="#">
+                        <li>About Me</li>
+                    </a>
+                    <a href="#">
+                        <li>Contact Me</li>
+                    </a>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Top-Bar'
+    
+}
+</script>
+
+<style lang="sass" scoped>
+    @use "./SASS/Header.sass"
+</style>
