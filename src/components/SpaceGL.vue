9b940127abc0634e0123e1c10f9efacc8158099f
interpreterK <rhpidfyre@tuta.io>
2023-01-18 10:55:16 -0500

    Implement github calendar for the site

--- src/components/SpaceGL.vue
+++ src/components/SpaceGL.vue
@@ -4,10 +4,18 @@
         <div class="Top-Bar-Window">
             <button id="Close-Prompt">X</button>
         </div>
+        <div class="gh-calendar">   
+             <!-- Loading stuff -->
+             Loading the data just for you.
+        </div>
     </div>
 </template>
 
 <script>
+import * as github_calendar from 'github-calendar'
+
+github_calendar('#WebGL_Renderer', "interpreterK")
+
 export default {
 	name: 'SpaceGL'
 }
